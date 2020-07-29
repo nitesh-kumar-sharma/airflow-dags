22b882660bbec435fa964831c9225eebe5894a94
Nitesh Sharma <sharma.nitesh590@gmail.com>
2020-07-29 11:43:19 +0530

    added sample dag 3

--- simple/tutorial_v03.py
+++ simple/tutorial_v03.py
@@ -6,7 +6,7 @@ from airflow.operators.python_operator import PythonOperator
 
 
 def print_world():
-    print('world_2')
+    print('world_3')
 
 
 default_args = {
@@ -16,17 +16,15 @@ default_args = {
 }
 
 
-with DAG('tutorial_v02',
+with DAG('tutorial_v03',
          default_args=default_args,
          schedule_interval='*/5 * * * *',
          ) as dag:
 
     print_hello = BashOperator(task_id='print_hello',
                                bash_command='echo "hello"')
-    sleep = BashOperator(task_id='sleep',
-                         bash_command='sleep 5')
     print_world = PythonOperator(task_id='print_world',
                                  python_callable=print_world)
 
 
-print_hello >> sleep >> print_world
+print_hello >> print_world
